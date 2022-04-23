7ecce9a445bf2c5eda0ec4fcc7a87edd81c5f0a0
SFONNE <apollohattsune@gmail.com>
2022-04-23 20:45:53 +0700

    Login

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Navbar = () => (
+    <div>
+        <Link href="/"><a> Home </a></Link> |
+        <Link href="/register"><a> Register </a></Link>  |
+        <Link href="/login"><a> Login </a></Link> |
+    </div>
+)
+
+export default Navbar
